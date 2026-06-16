08463b890ed195775ba297539d5003ef1d2a0c22
Sadman Ishraf Al Goni <Sadmanishraf100@gmail.com>
2026-06-16 11:38:05 +0600

    Initial commit

---
+++ src/main/java/com/summer26/section1/group10/simulatingoperationsofbangladeshfootballfederation/Launcher.java
@@ -0,0 +1,9 @@
+package com.summer26.section1.group10.simulatingoperationsofbangladeshfootballfederation;
+
+import javafx.application.Application;
+
+public class Launcher {
+    public static void main(String[] args) {
+        Application.launch(HelloApplication.class, args);
+    }
+}
---
+++ src/main/java/module-info.java
@@ -0,0 +1,8 @@
+module com.summer26.section1.group10.simulatingoperationsofbangladeshfootballfederation {
+    requires javafx.controls;
+    requires javafx.fxml;
+
+
+    opens com.summer26.section1.group10.simulatingoperationsofbangladeshfootballfederation to javafx.fxml;
+    exports com.summer26.section1.group10.simulatingoperationsofbangladeshfootballfederation;
+}
