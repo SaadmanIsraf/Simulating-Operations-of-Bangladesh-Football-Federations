14df7d821fa04674377c834428684dd35735154e
turjo <tahseenturjo313@gmail.com>
2026-07-28 02:43:19 +0600

    One user 8 goal fxml

--- src/main/java/com/summer26/section1/group10/simulatingoperationsofbangladeshfootballfederation/HelloApplication.java
+++ src/main/java/com/summer26/section1/group10/simulatingoperationsofbangladeshfootballfederation/HelloApplication.java
@@ -10,7 +10,7 @@ import java.io.IOException;
 public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/summer26/section1/group10/simulatingoperationsofbangladeshfootballfederation/turjo/federation_administrator/dashboardView.fxml"));
+        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/summer26/section1/group10/simulatingoperationsofbangladeshfootballfederation/turjo/federation_administrator/dashboardview.fxml"));
         Scene scene = new Scene(fxmlLoader.load());
         stage.setTitle("Hello!");
         stage.setScene(scene);
