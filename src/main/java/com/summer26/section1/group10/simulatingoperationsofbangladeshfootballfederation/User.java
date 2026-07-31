45d783d05a73e07af11a7c575159e106f8c9763c
Arman506li <arianislam3000@gmail.com>
2026-07-31 19:47:42 +0600

    Running test on scene Switching

--- src/main/java/com/summer26/section1/group10/simulatingoperationsofbangladeshfootballfederation/User.java
+++ src/main/java/com/summer26/section1/group10/simulatingoperationsofbangladeshfootballfederation/User.java
@@ -7,13 +7,63 @@ public abstract class User {
     protected String password;
     protected String role;
 
-
     public User(int id, String name, String email, String password, String role) {
         this.id = id;
         this.name = name;
         this.email = email;
         this.password = password;
         this.role = role;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    @Override
+    public String toString() {
+        return "User{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", password='" + password + '\'' +
+                ", role='" + role + '\'' +
+                '}';
     }
 
     //getter getter to string
