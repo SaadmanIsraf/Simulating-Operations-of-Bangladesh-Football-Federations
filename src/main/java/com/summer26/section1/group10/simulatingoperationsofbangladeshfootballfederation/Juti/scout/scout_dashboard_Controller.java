ce2ff6caef70816247af7f2f1778b7a8762c6f19
HabibaJuti <jutiummayhabiba@gmail.com>
2026-08-06 04:52:00 -0700

    update

---
+++ src/main/java/com/summer26/section1/group10/simulatingoperationsofbangladeshfootballfederation/Juti/scout/scout_dashboard_Controller.java
@@ -0,0 +1,42 @@
+package com.summer26.section1.group10.simulatingoperationsofbangladeshfootballfederation.Juti.scout;
+
+import javafx.event.ActionEvent;
+
+public class scout_dashboard_Controller
+{
+    @javafx.fxml.FXML
+    public void initialize() {
+    }
+
+    @javafx.fxml.FXML
+    public void validateReportButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void updateReportButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void recordPerformanceButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void searchPlayersButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void generatSummaryButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void createReportButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void viewAssignedMatchesButton(ActionEvent actionEvent) {
+    }
+
+    @javafx.fxml.FXML
+    public void recommenPlayersButton(ActionEvent actionEvent) {
+    }
+}
